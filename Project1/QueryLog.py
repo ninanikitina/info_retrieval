cd2620efc103b3b1fea9fa7ef44789de143654ef
Brandon Brugman <brandonbrugman@u.boisestate.edu>
2021-10-08 16:02:50 -0600

    Updates to query stuff again. Switched from list to dictionary.

--- Project1/QueryLog.py
+++ Project1/QueryLog.py
@@ -5,10 +5,12 @@ class Query(object):
         self.id = id
         self.query = query
         self.timestamp = timestamp
+        self.keyword = query.split()[0]
 
-    def print():
+    def print(self):
         print(f"Query ID: {self.id} | Timestamp: {self.timestamp}")
-        print(f"Query: {query}")
+        print(f"Query: {self.query}")
+        print("--------------------------------------------------")
 
 
 
