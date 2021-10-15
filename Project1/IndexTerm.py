3e65a13991f1c612b37863e56da5a8af73c92591
Nina <nninanikitina@gmail.com>
2021-10-14 21:05:26 -0600

    change algorithm for finding a subset of documents

--- Project1/IndexTerm.py
+++ Project1/IndexTerm.py
@@ -1,16 +1,4 @@
-from nltk.tokenize import RegexpTokenizer
-import pandas as pd
-import csv
-import time
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus.reader.wordnet import VERB, NOUN, ADJ, ADV
-from nltk.tag import StanfordPOSTagger
-from nltk.corpus import stopwords
 from tqdm import tqdm
-from functools import lru_cache
-from collections import defaultdict
-import matplotlib.pyplot as plt
-import pickle
 from collections import Counter
 import math
 
@@ -21,43 +9,83 @@ class IndexTerm(object):
         self.doc_max_term = self._build_doc_max(tokenized_df)
         self.docs_num = len(tokenized_df.index)
 
+
     def get_terms(self):
         return self.term_freq.keys()
 
 
     def _build_index(self, tokenized_df):
+        """
+        Build and return an index_term dictionary with the following structure:
+        {
+        term_1: {doc_id_i: frequency, ... , doc_id_n: frequency}
+        term_2: {doc_id_i: frequency ... , doc_id_n: frequency}
+        ...
+        term_n: {doc_id_i: frequency, ... , doc_id_n: frequency}
+        }
+        where doc_id_i is id of document that has specified term
+        and corresponding frequency is how many times the term appears in the document
+        Parameters
+            ----------
+            tokenized_df : df
+                Data frame with 2 columns:
+                "document_id" (int) | "all_content" (list of tokens)
+        """
         content = tokenized_df["all_content"].to_list()
         id = tokenized_df["id"].to_list()
         index_terms = {}
-        print("********************************")
-        print("Build index")
         for i, terms in enumerate(tqdm(content)):
             for term in terms:
                 if term in index_terms:
                     index_terms[term][id[i]] = index_terms[term].get(id[i], 0) + 1
                 else:
                     index_terms[term] = {id[i]: 1}
-        # need to save so we can use it later
         return index_terms
 
+
     def _build_doc_max(self, tokenized_df):
-        '''
-         number of times the most frequently-occurred term appears in d
-        :param tokenized_df:
-        :return: doc_max_term
-        '''
+        """
+        Build and return an index_term dictionary with the following structure:
+        { doc_id_1: max, doc_id_2: max, ... , doc_id_n: max }
+
+        where max  is the number of times the most frequently-occurred term appears in the document
+        Parameters
+            ----------
+            tokenized_df : df
+                Data frame with 2 columns:
+                "document_id" (int) | "all_content" (list of tokens)
+        """
         content = tokenized_df["all_content"].to_list()
         id = tokenized_df["id"].to_list()
         doc_max_term = {}
-        print("********************************")
-        print("Build max words")
         for i, terms in enumerate(tqdm(content)):
             c = Counter(terms)
-            max_value = max(c.values()) if len(c) is not 0 else 0
+            max_value = max(c.values()) if len(c) != 0 else 0
             doc_max_term[id[i]] = max_value
         return doc_max_term
 
+
     def rank_docs(self, query, max_num):
+        """
+        Choose a set of documents CR comprised of all the documents in DC
+        that contain each of the terms in q.
+        If there are less than 50 documents, then consider resources in DC
+        that contain n-1 terms in q (all the combinations).
+        Calculate relevance score for each document and return five most rated documents.
+        The 𝑅𝑒𝑙𝑒𝑣𝑎𝑛𝑐𝑒𝑆𝑐𝑜𝑟𝑒(𝑞, 𝑑) = ∑ 𝑇𝐹 (𝑤, 𝑑) × 𝐼𝐷𝐹(𝑤)
+        𝑇𝐹(𝑤, 𝑑) = 𝑓𝑟𝑒𝑞(𝑤, 𝑑) / 𝑚𝑎𝑥_d
+            freq(w, d) is the number of times w appears in the document
+            max_d is the number of times the most frequently-occurred term appears in the document
+        𝐼𝐷𝐹(𝑤) = 𝑙𝑜𝑔2(𝑁/𝑛)
+            N is the number of documents in DC
+            n is the number of documents in DC in which w appears at least once
+
+            ----------
+            tokenized_df : df
+                Data frame with 2 columns:
+                "document_id" (int) | "all_content" (list of tokens)
+        """
+        #  Calculate 𝐼𝐷𝐹(𝑤) = 𝑙𝑜𝑔2(𝑁/𝑛) for each term in the query
         idf_words = []
         for w in query:
             if w in self.term_freq:
@@ -66,14 +94,39 @@ class IndexTerm(object):
                 idf_words.append(idf_w)
             else:
                 idf_words.append(0)
-        doc_scores = {}
-        all_docs = set()
+
+        #  Choose a set of documents CR comprised of all the documents in DC
+        #  that contain each of the terms in q.
+        all_docs_id = set()
+        chosen_docs_id = set()
+        docs_term_sets = []
         for i, w in enumerate(query):
             if w in self.term_freq:
                 docs = set(k for k, v in self.term_freq[w].items())
-                all_docs.update(docs)
+                temp = docs.copy()
+                docs_term_sets.append(temp)
+                if i == 0:
+                    chosen_docs_id = docs
+                else:
+                    chosen_docs_id.intersection_update(docs)
+                all_docs_id.update(docs)
+        #  If there are less than 50 documents, then consider resources in DC
+        #  that contain n-1 terms in q (all the combinations), then n-2 ect.
+        min_q = len(query) - 1
+        while len(chosen_docs_id) < 50 and min_q >= 1:
+            all_docs_id.difference_update(chosen_docs_id)
+            for d in all_docs_id:
+                count = 0
+                for s in docs_term_sets:
+                    if d in s:
+                        count = count + 1
+                if count >= min_q:
+                    chosen_docs_id.add(d)
+            min_q = min_q - 1
 
-        for d in all_docs:
+        #  𝑇𝐹(𝑤, 𝑑) = 𝑓𝑟𝑒𝑞(𝑤, 𝑑) / 𝑚𝑎𝑥_d
+        doc_scores = {}
+        for d in chosen_docs_id:
             max_d = self.doc_max_term.get(d)
             relevance_score = 0
             for i, w in enumerate(query):
@@ -81,7 +134,8 @@ class IndexTerm(object):
                     continue
                 frequency_w_d = self.term_freq.get(w).get(d)
                 tf_w_d = frequency_w_d / max_d if frequency_w_d is not None else 0
-                relevance_score += tf_w_d * idf_words[i]
+                #  𝑅𝑒𝑙𝑒𝑣𝑎𝑛𝑐𝑒𝑆𝑐𝑜𝑟𝑒(𝑞, 𝑑) = ∑ 𝑇𝐹(𝑤, 𝑑) × 𝐼𝐷𝐹(𝑤)
+                relevance_score = relevance_score + tf_w_d * idf_words[i]
             doc_scores[d] = relevance_score
         element_num = max_num if max_num < len(doc_scores) else len(doc_scores)
 
--- Project1/main.py
+++ Project1/main.py
@@ -1,23 +1,20 @@
 import time
 import pickle
 import pandas as pd
-from preprocessing import tokanize_text
-from GenerateSnippets_v2 import GenerateSnippets
-from QueryLogger import QueryLogger
+from Project1.preprocessing import tokanize_text
+from Project1.GenerateSnippets_v2 import GenerateSnippets
+from Project1.QueryLogger import QueryLogger
+
 
 class SearchEngineData():
     def __init__(self):
         self.query_logger = QueryLogger()
 
 
-
-
-
-
     def run_query(self, query_sent="No query submitted"):
         if query_sent == "No query submitted":
             return "No query submitted"
-        
+        print("Uploading data...")
         corpus_df_name = "preprocessed_files/wiki_df.ftr"         # TODO: https://drive.google.com/drive/folders/1rSeosE42x1R4yW014VgmFhJGA5Yj8qOr?usp=sharing
         index_file_name = "preprocessed_files/wiki_index.pickle"  # TODO: Download file from google drive and move to the preprocessed_files folder in the Project1 directory
         results_file_name = "preprocessed_files/rank.csv"
@@ -25,11 +22,14 @@ class SearchEngineData():
         with open(index_file_name, 'rb') as handle:
             index = pickle.load(handle)
         df = pd.read_feather(corpus_df_name, columns=None, use_threads=True)
+        print("Data successfully uploaded")
 
-        
+        print("Building rank...")
         query = tokanize_text(query_sent, lower=True, remove_digits=True, is_lemmatized=True, remove_stop_words=True)
         rank = index.rank_docs(query, max_num=5)
+        print("Rank is created")
 
+        print("Generating snippets...")
         snippet_obj = GenerateSnippets(index.get_terms(), df)
         snippets_df = snippet_obj.getSnippets(query_sent, rank)
         snippets_df.to_csv(results_file_name)
@@ -37,6 +37,10 @@ class SearchEngineData():
         print(snippets_df)
 
 
-if __name__ == "__main__":
+def main():
     tester = SearchEngineData()
-    tester.run_query(query_sent = "TEST TEST TEST")
+    tester.run_query(query_sent="Boise Idaho")
+
+if __name__ == "__main__":
+    main()
+
