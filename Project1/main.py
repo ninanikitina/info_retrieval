5501ff77467ea7a090a74c47025db2bc619bbefb
Nina <nninanikitina@gmail.com>
2021-10-19 17:55:43 -0600

    fix bag

--- Project1/main.py
+++ Project1/main.py
@@ -5,9 +5,10 @@ import json
 #from Project1.preprocessing import tokanize_text
 #from Project1.GenerateSnippets_v2 import GenerateSnippets
 #from Project1.QueryLogger import QueryLogger
-from preprocessing import tokanize_text
-from GenerateSnippets_v2 import GenerateSnippets
-from QueryLogger import QueryLogger
+from Project1.preprocessing import tokanize_text
+from Project1.GenerateSnippets_v2 import GenerateSnippets
+from Project1.QueryLogger import QueryLogger
+
 
 
 class SearchEngineData():
@@ -26,13 +27,13 @@ class SearchEngineData():
         # results_file_name = "preprocessed_files/rank.csv"
 
         ## Stephs filepaths
-        #myp = "C:\\Users\\steph\\source\\repos\\info_retrieval\\Project1\\"
-        #corpus_df_name = myp + "preprocessed_files\\wiki_df.ftr"
-        #index_file_name = myp + "preprocessed_files\\wiki_index.pickle"
-        #results_file_name = myp + "preprocessed_files\\rank.csv"
+        myp = ""
+        corpus_df_name = myp + "preprocessed_files\\wiki_df.ftr"
+        index_file_name = myp + "preprocessed_files\\wiki_index.pickle"
+        results_file_name = myp + "preprocessed_files\\rank.csv"
 
-        corpus_df_name = "wiki_df.ftr"         # TODO: https://drive.google.com/drive/folders/1rSeosE42x1R4yW014VgmFhJGA5Yj8qOr?usp=sharing
-        index_file_name = "wiki_index.pickle"  # TODO: Download file from google drive and move to the preprocessed_files folder in the Project1 directory
+        # corpus_df_name = "wiki_df.ftr"         # TODO: https://drive.google.com/drive/folders/1rSeosE42x1R4yW014VgmFhJGA5Yj8qOr?usp=sharing
+        # index_file_name = "wiki_index.pickle"  # TODO: Download file from google drive and move to the preprocessed_files folder in the Project1 directory
         results_file_name = "rank.csv"
 
         with open(index_file_name, 'rb') as handle:
@@ -42,7 +43,7 @@ class SearchEngineData():
 
         print("Building rank...")
         query = tokanize_text(query_sent, lower=True, remove_digits=True, is_lemmatized=True, remove_stop_words=True)
-        all_rank, rank = index.rank_docs(query, max_num=5)
+        rank, all_rank = index.rank_docs(query, max_num=5)
         print(all_rank)
         print(rank)
         print("Rank is created")
@@ -51,6 +52,7 @@ class SearchEngineData():
         snippet_obj = GenerateSnippets(index.get_terms(), df)
         snippets_df = snippet_obj.getSnippets(query_sent, rank)
         #snippets_df.to_csv(results_file_name)
+        print("Snippets generated")
 
         snippets_json = json.dumps(snippets_df, indent = 4)
 
